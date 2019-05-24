7c03b04d66d82a09c2797afb25dd9bf8b515398a
gutsytechster <prashantsharma161198@gmail.com>
2019-05-24 13:50:37 +0530

    feat(users): Add fields to custom user and change admin accordingly
    This adds the fields to custom user model as well as to the fieldsets
    to be shown in the admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-# from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
@@ -13,14 +12,15 @@ class CustomUserAdmin(UserAdmin):
     list_display = ('email', 'is_staff', 'is_active',)
     list_filter = ('email', 'is_staff', 'is_active',)
     fieldsets = (
-        (None, {'fields': ('email', 'password',)}),
+        (None, {'fields': ('email', 'password', 'first_name',
+                           'last_name', 'phone_number')}),
         ('Permissions', {'fields': ('is_staff', 'is_active')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'is_staff',
-                       'is_active')
+            'fields': ('email', 'password1', 'password2', 'first_name',
+                       'last_name', 'phone_number', 'is_staff', 'is_active')
         }),
     )
     search_fields = ('email',)
--- users/models.py
+++ users/models.py
@@ -8,9 +8,9 @@ from .managers import CustomUserManager
 class CustomUser(AbstractUser):
     username = None
     email = models.EmailField(_('email address'), unique=True)
-    first_name = models.CharField(max_length=255, blank=False)
-    last_name = models.CharField(max_length=255, blank=False)
-    phone_number = models.CharField(max_length=15, blank=False)
+    first_name = models.CharField(max_length=255, blank=True)
+    last_name = models.CharField(max_length=255, blank=True)
+    phone_number = models.CharField(max_length=15, blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
@@ -18,4 +18,4 @@ class CustomUser(AbstractUser):
     objects = CustomUserManager()
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return self.email
