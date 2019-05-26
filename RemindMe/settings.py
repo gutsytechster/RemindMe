27f54b8905ef51bb49c6c4cfbb8e8745136c6179
gutsytechster <prashantsharma161198@gmail.com>
2019-05-26 22:53:02 +0530

    refactor(settings.py): Define Authentication settings and update installed_app

--- RemindMe/settings.py
+++ RemindMe/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'phonenumber_field',
     'rest_framework',
+    'rest_framework.authtoken',
     'Event',
     'users',
 ]
@@ -123,3 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
