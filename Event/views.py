60a7dc666d264da31debe5e0669c2d2012b0a9c3
gutsytechster <prashantsharma161198@gmail.com>
2019-05-26 23:02:58 +0530

    feat(Event): Detach user views and add customized event permissions

--- Event/views.py
+++ Event/views.py
@@ -1,30 +1,24 @@
-# from django.contrib.auth.models import User
-from users.models import CustomUser
 from rest_framework import generics
+from rest_framework import permissions
 
-from .serializers import EventSerializer, UserSerializer
+from .permissions import IsOwnerOrReadOnly
+from .serializers import EventSerializer
 from .models import Event
 
 
 class CreateView(generics.ListCreateAPIView):
     """This view performs POST http request to our api"""
-    queryset = Event.objects.all()
     serializer_class = EventSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+        return Event.objects.filter(owner=user)
 
 
 class DetailsView(generics.RetrieveUpdateDestroyAPIView):
     """This view performs GET, PUT and DELETE http requests to our api"""
     queryset = Event.objects.all()
     serializer_class = EventSerializer
-
-
-class UserListView(generics.ListAPIView):
-    """This view lists all the users in the database"""
-    queryset = CustomUser.objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetailView(generics.RetrieveAPIView):
-    """This view retrieves the information of the user"""
-    queryset = CustomUser.objects.all()
-    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticated,
+                          IsOwnerOrReadOnly,)
