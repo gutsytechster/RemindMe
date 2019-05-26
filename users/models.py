b3c02a3b61752d7c1ebb6d151e198e9817c5727e
gutsytechster <prashantsharma161198@gmail.com>
2019-05-26 22:46:59 +0530

    feat(users/models.py): Add fields for CustomUser and generate token for user
    This adds additonal fields for CustomUser and import respective modules.
    This also implements create_auth_token functionality which generates
    token for each user created.

--- users/models.py
+++ users/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 from .managers import CustomUserManager
 
@@ -8,9 +13,12 @@ from .managers import CustomUserManager
 class CustomUser(AbstractUser):
     username = None
     email = models.EmailField(_('email address'), unique=True)
-    first_name = models.CharField(max_length=255, blank=True)
-    last_name = models.CharField(max_length=255, blank=True)
-    phone_number = models.CharField(max_length=15, blank=True)
+    first_name = models.CharField(_('First Name'), max_length=255, blank=True,
+                                  null=False)
+    last_name = models.CharField(_('Last Name'), max_length=255, blank=True,
+                                 null=False)
+    phone_number = PhoneNumberField(_('Phone Number'), max_length=15,
+                                    blank=True, null=False, default='')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
@@ -18,4 +26,10 @@ class CustomUser(AbstractUser):
     objects = CustomUserManager()
 
     def __str__(self):
-        return self.email
+        return f"{self.email} - {self.first_name} {self.last_name}"
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
