dbee4c9527687cc7ccc5d287d63523cb34b92615
gutsytechster <prashantsharma161198@gmail.com>
2020-05-07 18:48:51 +0530

    fix(*): Update directory structure

--- src/RemindMe/local.py
+++ src/RemindMe/local.py
@@ -16,7 +16,7 @@ from datetime import timedelta
 import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = environ.Path(__file__) - 3
 
 env = environ.Env()
 env.read_env(os.path.join(BASE_DIR, ".env"))
